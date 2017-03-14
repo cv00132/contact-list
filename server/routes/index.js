035b76b23654106bc75adc30ff63e806de43c18a
Cory Vaughan <codercory13@gmail.com>
2017-03-14 14:30:53 -0400

    Added CORS to project.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
 // Require your controllers here
 const ContactController = require('../controllers/contacts');
 
+//Allows frontend to communicate to the backend
+ app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 module.exports = (app) => {
   // Add your routes here
