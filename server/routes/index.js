b02bf67176c46a961c5628a1024b36deb0c0f493
Cory Vaughan <codercory13@gmail.com>
2017-03-14 14:37:31 -0400

    Fixed CORS issue.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,16 @@
 // Require your controllers here
 const ContactController = require('../controllers/contacts');
 
+
+
+module.exports = (app) => {
+
 //Allows frontend to communicate to the backend
- app.use(function(req, res, next) {
+  app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-
-module.exports = (app) => {
   // Add your routes here
   app.post('/contacts', ContactController.create);
   app.get('/contacts', ContactController.listAll);
